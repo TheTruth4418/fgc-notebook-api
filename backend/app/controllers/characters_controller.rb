6f48bedba086442bbb03e42220aca06a0bc827d8
TheTruth4418 <68157179+TheTruth4418@users.noreply.github.com>
2021-05-20 15:11:07 -0500

    Folder Format changes

--- backend/app/controllers/characters_controller.rb
+++ backend/app/controllers/characters_controller.rb
@@ -1,2 +1,5 @@
 class CharactersController < ApplicationController
+    def index
+        
+    end
 end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  resources :notes
-  resources :matchup_notes
-  resources :matchups
-  resources :character_notes
-  resources :characters
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/characters' => 'characters#index'
 end
