742f600bbf6298fd0c388bc4cb6f77ede41893bc
TheTruth4418 <68157179+TheTruth4418@users.noreply.github.com>
2021-06-05 11:38:38 -0500

    Matchup Visuals updated

--- frontend/noteView.js
+++ frontend/noteView.js
@@ -54,9 +54,18 @@ class NoteView{
         let vs = document.createElement("label")
         let charSelect = document.createElement("select")
         let oppSelect = document.createElement("select")
+        let img1 = document.createElement("img")
+        let img1Div = document.createElement("div")
+        let vsImg = document.createElement("img")
+        let vsDiv = document.createElement("div")
+        let img2 = document.createElement("img")
+        let img2Div = document.createElement("div")
         let submit = document.createElement("input")
         submit.setAttribute("type", "submit");
 
+        img1Div.classList="column"
+        img2Div.classList="column"
+        vsDiv.classList="column"
         charSelect.name = "char"
         oppSelect.name = "opp"
         submit.value = "Search"
@@ -78,7 +87,17 @@ class NoteView{
         form.append(label1,charSelect,vs,oppSelect,submit)
             submit.addEventListener("click", function(event){ 
             event.preventDefault();
+            img1.src = `imgs/MK11/VS ${charSelect.value}.png`
+            img1.id = "charLeft"
+            vsImg.src  = `imgs/MK11/VS.png`
+            vsImg.id = "vs"
+            img2.src = `imgs/MK11/VS ${oppSelect.value}.png`
+            img2.id = "charRight"
             removeChildNodes(notesDiv)
+            img1Div.append(img1)
+            vsDiv.append(vsImg)
+            img2Div.append(img2)
+            cardImg.append(img1Div,vsDiv,img2Div)
             api.fetchMuNotes(charSelect.value, oppSelect.value);
         });
     }
